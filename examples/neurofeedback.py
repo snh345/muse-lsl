65d547e84a783ec6579c40ad024bfdc218062bae
snh345 <123588333+snh345@users.noreply.github.com>
2024-04-27 11:18:03 -0400

    Code
    My fork of muse-lsl that supports multi-stream sends

--- examples/neurofeedback.py
+++ examples/neurofeedback.py
@@ -18,6 +18,9 @@ import matplotlib.pyplot as plt  # Module used for plotting
 from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
 import utils  # Our own utility functions
 
+from pythonosc import udp_client
+import argparse
+
 # Handy little enum to make code more readable
 
 
@@ -30,6 +33,9 @@ class Band:
 
 """ EXPERIMENTAL PARAMETERS """
 # Modify these to change aspects of the signal processing
+
+alpha_buff1 = []
+alpha_buff2 = []
 
 # Length of the EEG data buffer (in seconds)
 # This buffer will hold last n seconds of data and be used for calculations
@@ -50,6 +56,14 @@ INDEX_CHANNEL = [0]
 
 if __name__ == "__main__":
 
+    """0. SET UP UDP CLIENT"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=5005, help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+
     """ 1. CONNECT TO EEG STREAM """
 
     # Search for active LSL streams
@@ -63,15 +77,22 @@ if __name__ == "__main__":
     inlet = StreamInlet(streams[0], max_chunklen=12)
     eeg_time_correction = inlet.time_correction()
 
+    inlet2 = StreamInlet(streams[1], max_chunklen=12)
+    eeg_time_correction2 = inlet.time_correction()
+
     # Get the stream info and description
     info = inlet.info()
     description = info.desc()
+
+    info2 = inlet2.info()
+    description2 = info2.desc()
 
     # Get the sampling frequency
     # This is an important value that represents how many EEG data points are
     # collected in a second. This influences our frequency band calculation.
     # for the Muse 2016, this should always be 256
     fs = int(info.nominal_srate())
+    fs2 = int(info2.nominal_srate())
 
     """ 2. INITIALIZE BUFFERS """
 
@@ -79,13 +100,20 @@ if __name__ == "__main__":
     eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
     filter_state = None  # for use with the notch filter
 
+    eeg_buffer2 = np.zeros((int(fs2 * BUFFER_LENGTH), 1))
+    filter_state2 = None  # for use with the notch filter
+
     # Compute the number of epochs in "buffer_length"
     n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
+                              SHIFT_LENGTH + 1))
+    
+    n_win_test2 = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
                               SHIFT_LENGTH + 1))
 
     # Initialize the band power buffer (for plotting)
     # bands will be ordered: [delta, theta, alpha, beta]
     band_buffer = np.zeros((n_win_test, 4))
+    band_buffer2 = np.zeros((n_win_test2, 4))
 
     """ 3. GET DATA """
 
@@ -101,27 +129,39 @@ if __name__ == "__main__":
             # Obtain EEG data from the LSL stream
             eeg_data, timestamp = inlet.pull_chunk(
                 timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+            eeg_data2, timestamp2 = inlet2.pull_chunk(
+                timeout=1, max_samples=int(SHIFT_LENGTH * fs2))
 
             # Only keep the channel we're interested in
             ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
+            ch_data2 = np.array(eeg_data2)[:, INDEX_CHANNEL]
 
             # Update EEG buffer with the new data
             eeg_buffer, filter_state = utils.update_buffer(
                 eeg_buffer, ch_data, notch=True,
                 filter_state=filter_state)
+            eeg_buffer2, filter_state2 = utils.update_buffer(
+                eeg_buffer2, ch_data2, notch=True,
+                filter_state=filter_state2)
 
             """ 3.2 COMPUTE BAND POWERS """
             # Get newest samples from the buffer
             data_epoch = utils.get_last_data(eeg_buffer,
                                              EPOCH_LENGTH * fs)
+            data_epoch2 = utils.get_last_data(eeg_buffer2,
+                                            EPOCH_LENGTH * fs2)
 
             # Compute band powers
             band_powers = utils.compute_band_powers(data_epoch, fs)
             band_buffer, _ = utils.update_buffer(band_buffer,
                                                  np.asarray([band_powers]))
+            band_powers2 = utils.compute_band_powers(data_epoch2, fs2)
+            band_buffer2, _ = utils.update_buffer(band_buffer2,
+                                                 np.asarray([band_powers2]))
             # Compute the average band powers for all epochs in buffer
             # This helps to smooth out noise
             smooth_band_powers = np.mean(band_buffer, axis=0)
+            smooth_band_powers2 = np.mean(band_buffer2, axis=0)
 
             # print('Delta: ', band_powers[Band.Delta], ' Theta: ', band_powers[Band.Theta],
             #       ' Alpha: ', band_powers[Band.Alpha], ' Beta: ', band_powers[Band.Beta])
@@ -135,6 +175,29 @@ if __name__ == "__main__":
                 smooth_band_powers[Band.Delta]
             print('Alpha Relaxation: ', alpha_metric)
 
+            alpha_metric2 = smooth_band_powers2[Band.Alpha] / \
+                smooth_band_powers2[Band.Delta]
+            print('Alpha Relaxation 2: ', alpha_metric2)
+
+            if (len(alpha_buff1) < 100):
+                alpha_buff1.append(alpha_metric)
+            else:
+                alpha_buff1.append(alpha_metric)
+                alpha_buff1 = alpha_buff1[1:]
+
+            if (len(alpha_buff2) < 100):
+                alpha_buff2.append(alpha_metric2)
+            else:
+                alpha_buff2.append(alpha_metric2)
+                alpha_buff2 = alpha_buff2[1:]
+
+            total = 0
+            for alpha1, alpha2 in zip(alpha_buff1, alpha_buff2):
+                error = (alpha1 - alpha2)**2
+                total += error
+
+            client.send_message("/diff", total)
+            # print("total difference: ", total);
             # Beta Protocol:
             # Beta waves have been used as a measure of mental activity and concentration
             # This beta over theta ratio is commonly used as neurofeedback for ADHD
---
+++ examples/neurofeedback_multi.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Estimate Relaxation from Band Powers
+
+This example shows how to buffer, epoch, and transform EEG data from a single
+electrode into values for each of the classic frequencies (e.g. alpha, beta, theta)
+Furthermore, it compares the alpha waves from the two threads (which are running the same stream).
+
+Adapted from https://github.com/NeuroTechX/bci-workshop
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pylsl import StreamInlet, resolve_byprop
+import utils
+import threading
+
+class Band:
+    Delta = 0
+    Theta = 1
+    Alpha = 2
+    Beta = 3
+
+""" EXPERIMENTAL PARAMETERS """
+BUFFER_LENGTH = 5
+EPOCH_LENGTH = 1
+OVERLAP_LENGTH = 0.8
+SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+
+# Index of the channel(s) (electrodes) to be used
+INDEX_CHANNEL = [0]  # Single channel
+
+last_100_samples_1 = []
+last_100_samples_2 = []
+
+def stream_data(stream_address, index_channel):
+    inlet = StreamInlet(stream_address, max_chunklen=12)
+    eeg_time_correction = inlet.time_correction()
+
+    info = inlet.info()
+    fs = int(info.nominal_srate())
+
+    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+    filter_state = None
+
+    n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) / SHIFT_LENGTH + 1))
+    band_buffer = np.zeros((n_win_test, 4))
+
+    try:
+        while True:
+            eeg_data, timestamp = inlet.pull_chunk(timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+            ch_data = np.array(eeg_data)[:, index_channel]
+            eeg_buffer, filter_state = utils.update_buffer(eeg_buffer, ch_data, notch=True, filter_state=filter_state)
+
+            data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs)
+            band_powers = utils.compute_band_powers(data_epoch, fs)
+            band_buffer, _ = utils.update_buffer(band_buffer, np.asarray([band_powers]))
+            smooth_band_powers = np.mean(band_buffer, axis=0)
+
+            alpha_metric = smooth_band_powers[Band.Alpha]
+            print(f'Alpha Relaxation for thread: ', alpha_metric)
+
+            # Store the last 100 samples
+            last_100_samples.extend(ch_data.flatten().tolist()[-100:])
+
+    except KeyboardInterrupt:
+        print('Closing!')
+
+def calculate_similarity(list1, list2):
+    assert len(list1) == len(list2), "Lists must have the same length"
+    similarity_sum = 0
+    for i in range(len(list1)):
+        similarity_sum += 1 - abs(list1[i] - list2[i]) / max(abs(list1[i]), abs(list2[i]), 1e-8)
+    similarity_value = similarity_sum / len(list1)
+    return similarity_value
+
+if __name__ == "__main__":
+    """ 1. CONNECT TO EEG STREAM """
+    print('Looking for an EEG stream...')
+    streams = resolve_byprop('type', 'EEG', timeout=2)
+    if len(streams) == 0:
+        raise RuntimeError('Can\'t find an EEG stream.')
+
+    stream_address = streams[0]
+
+    """ 2. START THREADS FOR THE SAME STREAM """
+    thread1 = threading.Thread(target=stream_data, args=(stream_address, INDEX_CHANNEL))
+    thread2 = threading.Thread(target=stream_data, args=(stream_address, INDEX_CHANNEL))
+
+    thread1.start()
+    thread2.start()
+
+    """ 3. COMPARE ALPHA WAVES """
+    last_100_samples_thread1 = thread1.join()
+    last_100_samples_thread2 = thread2.join()
+
+    similarity_value = calculate_similarity(last_100_samples_thread1, last_100_samples_thread2)
+    print(f'Similarity value: {similarity_value}')
+
+    print('Both threads have ended')
